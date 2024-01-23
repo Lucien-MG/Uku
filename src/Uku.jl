2d5d0c3ca0d87376d478f4a5a7cf6faa9a4512f6
Lucien Martin-Gaffe <lucien.martingaffe@gmail.com>
2024-01-23 09:00:50 +0100

    WIP: fix allocation.

--- src/Uku.jl
+++ src/Uku.jl
@@ -8,25 +8,29 @@ function play_tictactoe(game, move)
     end
 end
 
-function test2!(a:: Matrix{Int64}, b:: Matrix{Int64})
-    a .* b .* b .* b .* 1
+function test2!(a, b)
+    a .-= a .* b .* b .* b .* 1
 end
 
-function test()
-    a = [0 0 0; 0 0 0; 0 0 0]
-    b = [0 0 0; 0 0 0; 0 0 0]
-    for i=1:1000000
-        a .-= test2!(a,b)
+function test(a, b)
+    for i=1:100000
+        test2!(a, b)
     end
 end
 
 const game = TicTacToe()
-move = [0 0 0; 0 1 0; 0 0 0]
+const move = [0 0 0; 0 1 0; 0 0 0]
 
 @time play_tictactoe(game, move)
 @time play_tictactoe(game, move)
 
-@time test()
-@time test()
+a = [0 0 0; 0 0 0; 0 0 0]
+b = [0 0 0; 0 0 0; 0 0 0]
+
+@time test(a, b)
+@time test(a, b)
+
+@time test2!(a, b)
+@time test2!(a, b)
 
 end # module Uku
