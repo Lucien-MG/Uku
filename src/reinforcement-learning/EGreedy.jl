33b1b7344d664f14cece48c07f73da3c06b17dc4
Lucien Martin-Gaffe <lucien.martingaffe@gmail.com>
2024-02-22 09:45:21 +0100

    Fix reset agent.

--- src/reinforcement-learning/EGreedy.jl
+++ src/reinforcement-learning/EGreedy.jl
@@ -2,10 +2,13 @@ struct EGreedy
     epsilon::Float64
     alpha::Float64
 
+    nb_actions::Int64
+    optimistic_initial_value::Float64
+
     q_values::Array{Float64}
 
     function EGreedy(epsilon, alpha, nb_actions, optimistic_initial_value=0)
-        return new(epsilon, alpha, ones(nb_actions) * optimistic_initial_value)
+        return new(epsilon, alpha, nb_actions, optimistic_initial_value, ones(nb_actions) * optimistic_initial_value)
     end
 end
 
@@ -24,5 +27,5 @@ function learn(egreedy::EGreedy, action::Int64, reward::Float64)
 end
 
 function reset_agent(egreedy:: EGreedy)
-    egreedy.q_values .= 0
+    egreedy.q_values .= ones(egreedy.nb_actions) * egreedy.optimistic_initial_value
 end
